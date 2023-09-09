5161728d3916f66d84f2ff544d47729ba95205d3
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2023-09-09 21:29:23 +0200

    almost done project

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,10 @@ function kirajzol()
 {
     for(var i = 0;i<tomb.length;i++)
     {
-        
+        document.getElementById("alap").innerHTML += '<div id="keret'+i+'"> </div>'
+        document.getElementById("keret"+i+"").style.maxWidth = "tomb[i].elso"
+        document.getElementById("keret"+i+"").style.maxHeight = "tomb[i].mas"
+        document.getElementById("keret"+i+"").style.border = "thick solid black"
+        document.getElementById("alap").innerHTML+= '<br>'
     }
 }
