0af8165af5df8dcf316ad8e607604e32b6f273ec
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2023-09-09 21:16:53 +0200

    inputok és button

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function rajzol()
+{
+    var elso = document.getElementById("szam1").value
+    var mas = document.getElementById("szam2").value  
+    console.log(elso,mas)
+    
+    document.getElementById("alap").innerHTML = "<div > </div>"
+}
