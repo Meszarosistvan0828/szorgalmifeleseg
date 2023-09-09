03e5c7b4a42d52f6f87c85fcb0bb1890575d076f
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2023-09-09 22:04:24 +0200

    kész van elég sima volt <3

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@ function adat()
     var obj = { 
         elso : parseInt(document.getElementById("szam1").value),
         mas : parseInt(document.getElementById("szam2").value),
-    
         }
         tomb.push(obj)
     console.log(tomb)   
@@ -16,13 +15,14 @@ function adat()
 
 function kirajzol()
 {
+    document.getElementById("alap").innerHTML = ""
     for(var i = 0;i<tomb.length;i++)
     {
      
         document.getElementById("alap").innerHTML += '<div id="keret'+i+'"></div>'
-        document.getElementById("keret"+i+"").style.width = "tomb[i].elso'px'"
-        document.getElementById("keret"+i+"").style.height = "tomb[i].mas'px'"
-        document.getElementById("keret"+i+"").style.background = "black" 
+        document.getElementById("keret"+i+"").style.width = tomb[i].elso+"px"
+        document.getElementById("keret"+i+"").style.height =tomb[i].mas+"px"
+        document.getElementById("keret"+i+"").style.border = "solid 1px black" 
         document.getElementById("alap").innerHTML+= '<br>'
     }
 }
