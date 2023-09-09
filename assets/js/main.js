07749b3e4bfbd640a4b91a54b86145b3d1fb57c9
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2023-09-09 21:21:24 +0200

    még egy button és js functionok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,22 @@
-function rajzol()
+var tomb = []
+
+
+
+function adat()
 {
-    var elso = document.getElementById("szam1").value
-    var mas = document.getElementById("szam2").value  
-    console.log(elso,mas)
+    var obj = { 
+        elso : document.getElementById("szam1").value,
+        mas : document.getElementById("szam2").value ,
     
-    document.getElementById("alap").innerHTML = "<div > </div>"
+        }
+        tomb.push(obj)
+    console.log(tomb)   
+}
+
+function kirajzol()
+{
+    for(var i = 0;i<tomb.length;i++)
+    {
+        
+    }
 }
