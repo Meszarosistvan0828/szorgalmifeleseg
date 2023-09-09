d911eeafe6301a961be8f065205b12c699ec732a
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2023-09-09 21:56:31 +0200

    szopom a faszt

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,22 +5,24 @@ var tomb = []
 function adat()
 {
     var obj = { 
-        elso : document.getElementById("szam1").value,
-        mas : document.getElementById("szam2").value ,
+        elso : parseInt(document.getElementById("szam1").value),
+        mas : parseInt(document.getElementById("szam2").value),
     
         }
         tomb.push(obj)
     console.log(tomb)   
+
 }
 
 function kirajzol()
 {
     for(var i = 0;i<tomb.length;i++)
     {
-        document.getElementById("alap").innerHTML += '<div id="keret'+i+'"> </div>'
-        document.getElementById("keret"+i+"").style.maxWidth = "tomb[i].elso"
-        document.getElementById("keret"+i+"").style.maxHeight = "tomb[i].mas"
-        document.getElementById("keret"+i+"").style.border = "thick solid black"
+     
+        document.getElementById("alap").innerHTML += '<div id="keret'+i+'"></div>'
+        document.getElementById("keret"+i+"").style.width = "tomb[i].elso'px'"
+        document.getElementById("keret"+i+"").style.height = "tomb[i].mas'px'"
+        document.getElementById("keret"+i+"").style.background = "black" 
         document.getElementById("alap").innerHTML+= '<br>'
     }
 }
